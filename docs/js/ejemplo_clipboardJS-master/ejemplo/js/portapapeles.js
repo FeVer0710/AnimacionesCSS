be06a2f2a8218fc199b5f3ef5907c33f0a748262
Jaime Berríos <ingjaime1999@gmail.com>
2019-09-18 18:03:22 -0600

    Animaciones listas

--- docs/js/ejemplo_clipboardJS-master/ejemplo/js/portapapeles.js
+++
@@ -1,9 +0,0 @@
-var clipboard = new Clipboard('.portapapeles');
-
-clipboard.on('success', function(e) {
-    alert("COPIADO EXITOSO!");
-});
-
-clipboard.on('error', function(e) {
-	// Algo no salio como debia
-});
